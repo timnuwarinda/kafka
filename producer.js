62720b06745bd14a483565f9cbc73f4f8b97f32d
Timothy <hiitu7even@gmail.com>
2019-11-16 08:03:16 +0300

    new add

--- producer.js
+++ producer.js
@@ -2,48 +2,49 @@
 const express = require('express');
 const app = express();
 const Kafka = require('kafka-node');
-const config  = require('./config');
+const config = require('./config');
 
 const port = 8080;
 const Producer = Kafka.Producer;
-const client = new Kafka.KafkaClient({kafkaHost: config.KafkaHost});
-const producer = new Producer(client,  {requireAcks: 0, partitionerType: 2});
+const client = new Kafka.KafkaClient({ kafkaHost: config.KafkaHost });
+const producer = new Producer(client, { requireAcks: 0, partitionerType: 2 });
 
 
-const pushDataToKafka =(dataToPush) => {
+const pushDataToKafka = (dataToPush) => {
   console.log(`producer initialized........`);
 
   try {
-  let payloadToKafkaTopic = [{topic: config.KafkaTopic, messages: JSON.stringify(dataToPush) }];
-  console.log(payloadToKafkaTopic);
-  producer.on('ready', async function() {
-    producer.send(payloadToKafkaTopic, (err, data) => {
-          console.log('data: ', data);
-  });
+    let payloadToKafkaTopic = [{ topic: config.KafkaTopic, messages: JSON.stringify(dataToPush) }];
+    console.log(payloadToKafkaTopic);
+    producer.on('ready', async function () {
+      producer.send(payloadToKafkaTopic, (err, data) => {
+        console.log('data: ', data);
+      });
 
-  producer.on('error', function(err) {
-    console.log(err);
+      producer.on('error', function (err) {
+        console.log(err);
 
-    //  handle error cases here
-  })
-  })
+        //  handle error cases here
+      })
+    })
   }
-catch(error) {
-  console.log(error);
-}
+  catch (error) {
+    console.log(error);
+  }
 
 };
 
 
 const jsonData = require('./app_json.js');
+pushDataToKafka(jsonData);
 
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
 
-  pushDataToKafka(jsonData);
+  // pushDataToKafka(jsonData);
 
-     
+
 });
 
 app.listen(port);
